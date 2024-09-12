d8920b8676daf2de9cdca3d5c62e7554bfe2eb27
hyunji0126 <88438302+hyunji0126@users.noreply.github.com>
2024-09-12 13:25:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ onCustomWidgetDestroy(){
 }
 
 render(){
- this._root.textContent='Hello Custom Widget clientwidth : ${this.clientwidth}, clientHeight: ${this.clientHeight}'
+ this._root.textContent="Hello Custom Widget clientwidth : ${this.clientwidth}, clientHeight: ${this.clientHeight}"
 }
  }
 customElements.define('com-sap-sac-exercise-j002-main', Main)
